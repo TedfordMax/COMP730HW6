9c783a090e59bd5a924c5cf1d2462d38ba723bd7
TedfordMax <42852936+TedfordMax@users.noreply.github.com>
2020-03-08 21:12:24 -0400

    Add files via upload

---
+++ OrderDieTestCase.java
@@ -0,0 +1,15 @@
+import static org.junit.jupiter.api.Assertions.OrderDie;
+
+import org.junit.jupiter.api.Test;
+
+public class MyTests {
+
+    @Test
+    public void multiplicationOfZeroIntegersShouldReturnZero() {
+        OrderDie tester = new OrderDie();
+
+        OrderDie(0, tester.OrderDie("Steve", "blue"));
+        OrderDie(0, tester.OrderDie("Tom", "red"));
+        OrderDie(0, tester.OrderDie("Jace", "red yellow");
+    }
+}
